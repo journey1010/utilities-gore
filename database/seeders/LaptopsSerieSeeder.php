d5e9591507c388d3bba8b2021e14e81e1b225924
Journii <ginopaflo001608@gmail.com>
2024-02-13 10:44:54 -0500

    try catch para el seeder de maestros

--- database/seeders/LaptopsSerieSeeder.php
+++ database/seeders/LaptopsSerieSeeder.php
@@ -17,38 +17,45 @@ class LaptopsSerieSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {    
-        $caja = '';
-        $serie = '';
-        $condicion = '';
-
-        $archivo = storage_path('app/docs/2023/laptops.xlsx');
-        $reader = IOFactory::createReader('Xlsx');
-        $spreedsheet = $reader->load($archivo);
-        $worksheet = $spreedsheet->getActiveSheet();
-        $highestRow = $worksheet->getHighestRow();
-        $highestColumn = Coordinate::columnIndexFromString($this->maxColumn);
-
-        for($row = 2; $row <= $highestRow; $row++){
-            for($col = 1; $col <= $highestColumn; $col++){
-                $value  = $worksheet->getCell([$col, $row])->getValue();
-                switch($col){
-                    case 1:
-                        $caja = $value;
-                        break;
-                    case 2:
-                        $serie = $value;
-                        break;
-                    case 3:
-                        $condicion = $value;
-                        break;
+    {   
+        try {
+            $caja = '';
+            $serie = '';
+            $condicion = '';
+    
+            $archivo = storage_path('app/docs/2023/laptops.xlsx');
+            $reader = IOFactory::createReader('Xlsx');
+            $spreedsheet = $reader->load($archivo);
+            $worksheet = $spreedsheet->getActiveSheet();
+            $highestRow = $worksheet->getHighestRow();
+            $highestColumn = Coordinate::columnIndexFromString($this->maxColumn);
+    
+            for($row = 2; $row <= $highestRow; $row++){
+                for($col = 1; $col <= $highestColumn; $col++){
+                    $value  = $worksheet->getCell([$col, $row])->getValue();
+                    switch($col){
+                        case 1:
+                            $caja = $value;
+                            break;
+                        case 2:
+                            $serie = $value;
+                            break;
+                        case 3:
+                            $condicion = $value;
+                            break;
+                    }
                 }
+                Lap::create([
+                    'caja' => $caja,
+                    'serie' => $serie,
+                    'condicion' => $condicion
+                ]);
             }
-            Lap::create([
-                'caja' => $caja,
-                'serie' => $serie,
-                'condicion' => $condicion
-            ]);
+        } catch (\Exception $e) {
+            response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 500);
         }
     }
 }
